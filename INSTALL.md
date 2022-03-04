aa5bbf151eeeb342f7911d3a53eee8f66c9bc95b
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-03-04 17:37:38 +0100

    Add firebase.js

---
+++ INSTALL.md
@@ -0,0 +1,9 @@
+# INSTALLATION  
+* Node JS  
+- curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -  
+- sudo apt-get install -y nodejs  
+* Firebase  
+- Create new project "Neuralearn"  
+- Select Web Icon
+- npm install firebase
+- Go to Settings > Project Settings > SDK Setup and Configuration > Config & copy the config 
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDD4lStlanSUeWcHohYkGLugmVi2lNZ3_k",
+  authDomain: "neuralearn-e0a3a.firebaseapp.com",
+  projectId: "neuralearn-e0a3a",
+  storageBucket: "neuralearn-e0a3a.appspot.com",
+  messagingSenderId: "1003143612810",
+  appId: "1:1003143612810:web:cdb4a74160114f3835a073",
+  measurementId: "G-Y1ZXE1TRP7"
+};
