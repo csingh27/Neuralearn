ff9489d3a54f0f34b1bae176d2d38c067bc6b7bf
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-03-07 21:17:22 +0100

    Add Sidebar Rowspreliminary

--- neuralearn/src/Sidebar.js
+++ neuralearn/src/Sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import SidebarRow from './SidebarRow';
 import './Sidebar.css';
+import './Sidebar.js';
 
 function Sidebar(){
 return(
 	<div className="sidebar">
 	<h2> Sidebar here  </h2>
+	<SidebarRow title = "Home" />
+	<SidebarRow title = "Podcast" />
+	<SidebarRow title = "Meetup" />
+	<SidebarRow title = "Talks" />
 	</div>	
 );
 }
