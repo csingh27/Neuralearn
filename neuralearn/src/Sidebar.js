8d65697fb78aeeaa1f265ad8d6db96b565496fd0
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-03-07 22:08:01 +0100

    Update Sidebar icons

--- neuralearn/src/Sidebar.js
+++ neuralearn/src/Sidebar.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import MicIcon from '@material-ui/icons/Mic';
+import GradeIcon from '@material-ui/icons/Grade';
+import GroupIcon from '@material-ui/icons/Group';
+import HomeIcon from '@material-ui/icons/Home';
 import SidebarRow from './SidebarRow';
 import './Sidebar.css';
 import './Sidebar.js';
@@ -6,11 +10,10 @@ import './Sidebar.js';
 function Sidebar(){
 return(
 	<div className="sidebar">
-	<h2> Sidebar here  </h2>
-	<SidebarRow title = "Home" />
-	<SidebarRow title = "Podcast" />
-	<SidebarRow title = "Meetup" />
-	<SidebarRow title = "Talks" />
+	<SidebarRow Icon={HomeIcon} title = "Home" />
+	<SidebarRow Icon={GradeIcon} title = "Talks" />
+	<SidebarRow Icon={GroupIcon} title = "Meetup" />
+	<SidebarRow Icon={MicIcon} title = "Podcast" />
 	</div>	
 );
 }
